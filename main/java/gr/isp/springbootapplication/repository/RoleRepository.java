7d774e233447e49bf40342b647da53a7f583d404
Ek0n13 <alextgen@gmail.com>
2019-11-12 20:20:47 +0200

    remnoved extra main folder

--- main/java/gr/isp/springbootapplication/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package gr.isp.springbootapplication.repository;
-
-import gr.isp.springbootapplication.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-}
