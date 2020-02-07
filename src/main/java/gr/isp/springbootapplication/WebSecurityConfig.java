1ef035ce0e55c1222b7f49d086936c80130fa421
John Daniel <jondan97@gmail.com>
2020-02-07 20:09:57 +0200

    added e-mail notification sent to applicant applying, created firstTime method(admin role is inserted into the database for application operations to start working), removed unwanted dependencies from pom.xml

--- src/main/java/gr/isp/springbootapplication/WebSecurityConfig.java
+++ src/main/java/gr/isp/springbootapplication/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         http
                 .authorizeRequests()
                 .antMatchers(resources).permitAll()
-                .antMatchers("/","/login","/view/**", "/contactUs/**", "/error").permitAll()
+                .antMatchers("/", "/login", "/view/**", "/contactUs/**", "/error", "/firstTime").permitAll()
                 .antMatchers("/admin*").access("hasRole('ADMIN')")
                 .antMatchers("/user*").access("hasRole('USER') or hasRole('ADMIN')")
                 .anyRequest().authenticated()
